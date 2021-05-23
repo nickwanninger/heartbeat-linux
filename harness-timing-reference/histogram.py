5578e9301f622a9f7493266608564c534be4a733
deniz <d.ulusel@gmail.com>
2021-05-23 17:15:46 -0500

    refactor histogram script

--- harness-timing-reference/histogram.py
+++ harness-timing-reference/histogram.py
@@ -11,6 +11,7 @@ records = {} # the actual data
 data_encountered = False # have we read the line that says "data" yet?
 num_records_per_thread_encountered = False # same as above for "num_records_per_thread"
 
+print("Reading file...\n")
 with open(filepath) as fp:
 	for line_num, line in enumerate(fp):
 
@@ -47,35 +48,65 @@ with open(filepath) as fp:
 
 			if int(line_num) % num_max < record_lengths[cur_tid]:
 				records[cur_tid].append(int(line))
+print("\nDone reading file\n")
+### WE'VE READ THE DATA, NOW PROCESS
 
-# sanity check, ensure length of data read is correct
-for (tid, length) in enumerate(record_lengths):
-	assert length <= num_max
-	assert len(records[tid]) == length
+def sanity_check(record_lengths):
+	# ensure length of data read is correct
+	for (tid, length) in enumerate(record_lengths):
+		assert length <= num_max
+		assert len(records[tid]) == length
 
-# merge results from ALL threads
-all_intervals = []
-for k in records:
-	all_intervals += records[k]
-
-#### WE READ ALL DATA, NOW PROCESS
+def merge_threads_results(records):
+	# merge results from ALL threads
+	all_intervals = []
+	for k in records:
+		all_intervals += records[k]
+	return all_intervals
 
 def cycles_to_usec(cycles, cpu_ghz):
 	cycles_per_usec = cpu_ghz * 1000
 	return cycles / cycles_per_usec
 
-CPU_GHZ = 1.8 # rohiths cpu
-all_intervals_usec = list(map(lambda x: cycles_to_usec(x, CPU_GHZ), all_intervals))
-print("MEAN OF ALL INTERVALS: " + str(np.mean(all_intervals_usec)) + " usec")
+def convert_cycles_list_to_usec_list(cycles_list, cpu_ghz):
+	print("Converting cycles to usec with CPU speed: " + str(cpu_ghz) + " GHz\n")
+	return list(map(lambda x: cycles_to_usec(x, cpu_ghz), cycles_list))
 
-THRESHOLD_USEC = 30
-print("threshold " + str(THRESHOLD_USEC) + " cycles")
+def print_statistics(data_list, unit):
+	print("Mean: " + str(np.mean(data_list)) + " " + unit)
+	print("Min:  " + str(np.min(data_list)) + " " + unit)
+	print("Max:  " + str(np.max(data_list)) + " " + unit)
+	print()
 
-filtered_data = list(filter(lambda x: x < THRESHOLD_USEC, all_intervals_usec))
+def threshold_list(data_list, low, high, unit):
+	f1 = list(filter(lambda x: x <= high, data_list))
+	f2 = list(filter(lambda x: x >= low, f1))
+	n = len(f2)
+	print("Total data points: " + str(len(data_list)))
+	print("Data points between (both inclusive) " 
+		+ str(low) + unit + " and " + str(high) + unit + ": " + str(n))
+	print("Fraction is " + str(n/len(data_list)))
 
-print("number below threshold " + str(len(filtered_data)))
-print("total data points " + str(len(all_intervals_usec)))
-print("fraction is " + str(len(filtered_data)/len(all_intervals_usec)))
+sanity_check(record_lengths)
+all_intervals = merge_threads_results(records)
 
-fig = px.histogram(all_intervals)
+# Print number of zero data points to ensure data is not garbage
+num_zeros = len(list(filter(lambda x: x == 0, all_intervals)))
+print("Number of zero points: " + str(num_zeros))
+
+# CPU_GHZ = 1.8 # rohiths cpu
+CPU_GHZ = 3.3 # picard cpu
+all_intervals_usec = convert_cycles_list_to_usec_list(all_intervals, CPU_GHZ)
+print_statistics(all_intervals_usec, "usec")
+
+THRESHOLD_LOW_USEC = 0
+THRESHOLD_HIGH_USEC = 30
+threshold_list(all_intervals_usec, THRESHOLD_LOW_USEC, THRESHOLD_HIGH_USEC, "usec")
+
+fig = px.histogram(all_intervals_usec)
+fig.update_layout(
+    xaxis_title="interval (microseconds)",
+    yaxis_title="count"
+)
+# fig.update_xaxes(tick0=0, dtick=10)
 fig.show()
