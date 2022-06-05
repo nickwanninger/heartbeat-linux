26506471ec0427ccb6c7fe6f4dbc519a029667d1
mikerainey <bionic.oldsmobile@gmail.com>
2022-06-05 13:18:42 -0400

    bypass stack smashing detection

--- rf_compiler/run.sh
+++ rf_compiler/run.sh
@@ -5,8 +5,8 @@ set -e
 
 mkdir -p build
 # compile the source code to asm
-clang -O3 -g0 -I../include -S sum_array.c -o build/sum_array.s
+clang -O3 -g0 -fno-stack-protector -I../include -S sum_array.c -o build/sum_array.s
 # transform it
 python3 transform.py build/sum_array.s build/sum_array.rf.s
 # make a binary
-clang build/sum_array.rf.s ../src/entry.S -o build/sum_array
+clang build/sum_array.rf.s -fno-stack-protector ../src/entry.S -o build/sum_array
