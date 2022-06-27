8bdf995abca87c948ae9599b060151997e29e00d
mikerainey <bionic.oldsmobile@gmail.com>
2022-06-27 09:20:57 -0400

    edit

--- rf_compiler/run.sh
+++ rf_compiler/run.sh
@@ -5,7 +5,7 @@ set -e
 
 mkdir -p build
 # compile the source code to asm
-clang -O3 -g0 -I../include -S sum_array.c -o build/sum_array.s
+clang -O3 -g3 -I../include -S sum_array.c -o build/sum_array.s
 # transform it
 python3 transform.py build/sum_array.s build/sum_array.rf.s
 # make a binary
