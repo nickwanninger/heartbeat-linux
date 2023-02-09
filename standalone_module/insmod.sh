ed29cd232d0c8eac0ef9f5bcfef3f91102839a72
Nadharm <nadharmdhiantravn@gmail.com>
2023-02-08 20:26:09 -0600

    fixed insmod path in insmod.sh

--- standalone_module/insmod.sh
+++ standalone_module/insmod.sh
@@ -4,7 +4,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-insmod /etc/mod.ko \
+insmod ./mod.ko \
     hb_error_entry=0x$(grep ' error_entry' /proc/kallsyms | cut -d' ' -f1) \
     hb_error_return=0x$(grep ' error_return' /proc/kallsyms | cut -d' ' -f1)
 
