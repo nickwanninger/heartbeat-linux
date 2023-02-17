cb87bd6a57ca09c01fb9de6c97486310b0afaeff
mikerainey <bionic.oldsmobile@gmail.com>
2023-02-17 13:58:21 -0500

    adding support for nix

--- standalone_module/insmod.sh
+++ standalone_module/insmod.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo "This script must be run as root"
    exit 1
 fi
 
-insmod ./mod.ko \
+MOD=${1:-./mod.ko}
+
+insmod $MOD \
     hb_error_entry=0x$(grep ' error_entry' /proc/kallsyms | cut -d' ' -f1) \
     hb_error_return=0x$(grep ' error_return' /proc/kallsyms | cut -d' ' -f1)
 
 mknod /dev/heartbeat c 248 0
-
 
